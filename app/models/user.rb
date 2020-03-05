0646ee6a395c0de75d7ebade4a172ccad6e449df
yuigawa <57977700+yuigawa@users.noreply.github.com>
2020-03-05 14:32:27 +0900

    commit(Userテーブル)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :item_users
+  has_many :items through: :item_users
+  has_many :comments
+  has_many :favorites
+  has_one  :profile
+  has_one  :user_address
+  has_many  :credit_cards
 end
