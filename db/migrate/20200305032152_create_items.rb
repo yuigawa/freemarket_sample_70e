477cc65fa8a08b434664f67f62f6dec1979ddee9
thoughtake <sought1001@gmail.com>
2020-03-06 21:39:40 +0900

    change index file & migration file

--- db/migrate/20200305032152_create_items.rb
+++ db/migrate/20200305032152_create_items.rb
@@ -1,6 +1,7 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
+      t.string :name
       t.string :buyer
       t.string :size
       t.string :item_condition
