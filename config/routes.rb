6171b4f2361d1bec4295f7f0851600f871cfa482
thoughtake <sought1001@gmail.com>
2020-03-06 20:22:55 +0900

    change root to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'items#show'
+  root 'items#index'
   devise_for :users
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
