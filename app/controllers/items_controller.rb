faf84d9964bb1ec447b321e81a4fcca1001edb85
thoughtake <sought1001@gmail.com>
2020-03-09 19:48:32 +0900

    edit controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,9 +10,23 @@ class ItemsController < ApplicationController
     @brands = Brand.all
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :item_description, :category_id, :brand_id, :size, :item_condition, :postage_payer, :prefecture_code, :estimated_shipping_date, :price, item_images_attributes: [:src])
   end
 end
