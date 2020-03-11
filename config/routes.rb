850023dc390df07d2caac6e5e83354f35ef6f08f
yuigawa <57977700+yuigawa@users.noreply.github.com>
2020-03-11 18:35:48 +0900

    commit(サーバカテゴリ)
    カテゴリーボックスを動的に動かす

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items
   resources :tops
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products, only: [:index, :show, :new, :edit, :destroy] do
+    #Ajaxで動くアクションのルートを作成
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+end
 end
