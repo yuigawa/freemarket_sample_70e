57e7d7c16d6b6ece3338f71c390d87110990b6d9
yuigawa <57977700+yuigawa@users.noreply.github.com>
2020-03-11 20:02:33 +0900

    revert 12files

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items
   resources :tops
-  resources :products, only: [:index, :show, :new, :edit, :destroy] do
-    #Ajaxで動くアクションのルートを作成
-    collection do
-      get 'get_category_children', defaults: { format: 'json' }
-      get 'get_category_grandchildren', defaults: { format: 'json' }
-    end
-end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
