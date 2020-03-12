f6f13be7ce1fd4e8a41d963c59745f819f1fefaf
yuigawa <57977700+yuigawa@users.noreply.github.com>
2020-03-12 14:23:05 +0900

    親カテゴリー実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   root 'tops#index'
-  
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :items
   resources :tops
+  resources :products, only: [:index, :show, :new, :edit, :destroy] do
+    #Ajaxで動くアクションのルートを作成
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
