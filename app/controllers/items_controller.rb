33fa1cf3b488de3bcb357c60281d3485539d21b0
thoughtake <sought1001@gmail.com>
2020-03-12 14:12:59 +0900

    edit items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
+      @item.item_images.new if @item.item_images.empty?
       render :new
     end
   end
@@ -28,7 +29,7 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :item_description, :category_id, :brand_id, :size, :item_condition, :postage_type, :postage_payer, :prefecture_code, :estimated_shipping_date, :price, item_images_attributes: [:src, :src_cache]).merge(user_id: current_user.id, trading_status:"出品中")
+    params.require(:item).permit(:name, :item_description, :category_id, :brand_id, :size, :item_condition, :postage_type, :postage_payer, :prefecture_code, :estimated_shipping_date, :price, item_images_attributes: [:src]).merge(user_id: current_user.id, trading_status:"出品中")
   end
 
   def set_info
