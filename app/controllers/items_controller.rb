9b3a2ae017929ed9e25015aff7b5f88418e33a90
yuigawa <57977700+yuigawa@users.noreply.github.com>
2020-03-13 15:05:44 +0900

    commit indent
    インデントがおかしくなっていた箇所を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -53,7 +53,7 @@ class ItemsController < ApplicationController
   def destroy
     @item = Item.find(params[:id])
     if @item.destroy
-    redirect_to root_path
+      redirect_to root_path
     else
       alert('削除できませんでした')
     end
