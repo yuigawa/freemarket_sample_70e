5e14924702ce1c3fa60380f10803e7c4799058fe
takadamu <0rg68v3m326626f@gmail.com>
2020-03-14 00:05:16 +0900

    pay.jpコントローラー単数間違え

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'card/new'
+  # get 'card/show'
   root 'tops#index'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :tops
@@ -11,5 +13,6 @@ Rails.application.routes.draw do
   end
   resources :users
   resources :item_images
+  resources :card
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
