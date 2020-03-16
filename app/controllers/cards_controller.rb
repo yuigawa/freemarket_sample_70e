b62d5adaff6ce89fa918a6a4b56ec1a8fafb847e
takadamu <0rg68v3m326626f@gmail.com>
2020-03-16 19:35:21 +0900

    payjp登録削除修正

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,6 @@
 class CardsController < ApplicationController
   require "payjp"
+  before_action :set_card, only: [:show, :delete]
 
   def new
     card = Card.where(user_id: current_user.id)
@@ -27,25 +28,22 @@ class CardsController < ApplicationController
   end
 
   def delete #PayjpとCardデータベースを削除します
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
-    else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
-      card.delete
-    end
-      redirect_to action: "new"
+      @card.delete
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
-      redirect_to action: "new" 
-    else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+  end
+
+  def set_card
+    @card = Card.where(user_id: current_user.id).first
+    unless @card.present?
+      redirect_to action: "new"
     end
   end
 end
