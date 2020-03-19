994ff0cbfc30c94df5c66e3a1a946e6375beede6
nogasho1122 <exile1122sho@icloud.com>
2020-03-19 17:47:43 +0900

    update#2

--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -4,4 +4,5 @@ class UserAddress < ApplicationRecord
 
   # 住所
   validates :post_code,:prefecture_code,:city, :house_number, presence: true
+  validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, multiline: true}
 end
