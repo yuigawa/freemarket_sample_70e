45412580318a7eaed1f8726b2d6ac6ec87ac5e61
yuigawa <57977700+yuigawa@users.noreply.github.com>
2020-03-19 17:26:07 +0900

    commit

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,18 @@
 class PurchasesController < ApplicationController
   def new
-    @item = Item.find(params[:item_id])   
+    @item = Item.find(params[:item_id]) 
+    @user = User.find(current_user.id)
+    #Cardのデータpayjpに送り情報を取り出します
+    @card = Card.where(user_id: current_user.id).first
+    if @card
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
   end
   def create
+    item = Item.find(params[:item_id])
+    item.update(trading_status: "購入済", buyer_id: current_user.id)
   end
+
 end
