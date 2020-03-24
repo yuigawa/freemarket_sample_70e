8a8a9de1dda89661fb281cf93d4954bf44e01026
yuigawa <57977700+yuigawa@users.noreply.github.com>
2020-03-24 19:00:39 +0900

    commit

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :user
+
+
+  validates :customer_id, presence: true, presence: true
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#クレカが登録してあれば購入できる
+describe  User do
+  describe '#card' do
+
+    it "is invalid without a customer_id" do
+      card = build(:user, customer_id: "")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("can't be blank")
+    end
+
+    it "is invalid without an card_id" do
+      card = build(:user, card_id: "")
+      card.valid?
+      expect(user.errors[:card_id]).to include("can't be blank")
+    end
+
+  end
 end
